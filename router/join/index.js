f5fa9e8ed837603053b1504caa30caab62edd1aa
Abraham <wits73@hotmail.com>
2018-07-01 17:35:50 -0400

    inserted data to db

--- router/join/index.js
+++ router/join/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var path = require('path')
 var mysql = require('mysql')
-
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1qaz2wsX';
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -14,7 +14,27 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.get('/', function(req, res){
+    console.log("get join url");
     res.sendFile(path.join(__dirname,'../../public/join.html'))
+});
+
+router.post('/', function(req, res){
+    
+    var body = req.body;
+    var email = req.email;
+    var name = req.name;
+    var pw = req.pw;
+    // https://github.com/mysqljs/mysql#escaping-query-values
+    var sql = {email : email, name : name, pw : pw}
+    var query = connection.query('insert into user set ?', sql, function(err, rows){
+        if(err)
+            {throw err;}
+        else{
+            console.log('test test');
+            res.render('welcome.ejs', {'name' : name, 'id':rows.insertId});
+        }
+           
+    });
 });
 
 
